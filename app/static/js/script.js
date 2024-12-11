3801529786f766be1644e8965eb4f240afd1f2b2
gabi-carbon <gacuna.inf@gmail.com>
2024-12-11 20:05:32 +0100

    some fixes

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -115,7 +115,10 @@ function onScanFailure(error) {
 }
 
 let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
+    "reader",{
+        fps: 10,
+        qrbox: {width: 250, height: 250},  
+        facingMode: { exact: "environment"}
+    },
     /* verbose= */ false);
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
